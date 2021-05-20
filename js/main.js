7d5f60cc7fd4764d62c9ee6aaeedcaf6dd2efc60
Brad Meyn <bradmeyn@Brads-MacBook-Air.local>
2021-05-20 21:32:23 +1000

    standardise font, fix navbar on mobile

--- js/main.js
+++ js/main.js
@@ -17,17 +17,17 @@ content.addEventListener("click", ()=> {
 
 
 
-/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
-let prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  let currentScrollPos = window.pageYOffset;
-  if(window.innerWidth < 750){
-    if (prevScrollpos > currentScrollPos) {
-      document.getElementById("header").style.top = "0";
-    } else {
-      document.getElementById("header").style.top = "-100px";
-    }
-    prevScrollpos = currentScrollPos;
-  }
+// /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+// let prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   let currentScrollPos = window.pageYOffset;
+//   if(window.innerWidth < 750){
+//     if (prevScrollpos > currentScrollPos) {
+//       document.getElementById("header").style.top = "0";
+//     } else {
+//       document.getElementById("header").style.top = "-100px";
+//     }
+//     prevScrollpos = currentScrollPos;
+//   }
   
-}
+// }
