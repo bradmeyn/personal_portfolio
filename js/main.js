18c3731016b1bd36d7b7973cbcba5952c9d33733
Brad Meyn <bradjmeyn@gmail.com>
2021-06-04 10:20:30 +1000

    added small projects

--- js/main.js
+++ js/main.js
@@ -3,85 +3,84 @@
 
 
 
-
 let ham = document.querySelector(".hamburger");
 const body = document.querySelector("body");
 const content = document.querySelector(".content");
 
-
-  
-  ham.addEventListener("click", () => {
-ham.classList.toggle("active");
+ham.addEventListener("click", () => {
+  ham.classList.toggle("active");
   body.classList.toggle("blurry");
 });
+
 
 //
 
 window.addEventListener("resize", () => {
-  if(window.innerWidth > 760){
+  if (window.innerWidth > 760) {
     ham.classList.remove("active");
-      body.classList.remove("blurry");
+    body.classList.remove("blurry");
   }
 });
 
-
-content.addEventListener("click", ()=> {
-  
-    ham.classList.remove("active");
-    body.classList.remove("blurry");
+content.addEventListener("click", () => {
+  ham.classList.remove("active");
+  body.classList.remove("blurry");
 });
 
 const links = document.querySelectorAll(".navbar__link-container");
 
 links.forEach((link) => {
-  link.addEventListener("click",()=> {
-    if(window.innerWidth < 768) {
+  link.addEventListener("click", () => {
+    if (window.innerWidth < 768) {
       ham.classList.toggle("active");
-  body.classList.toggle("blurry");
+      body.classList.toggle("blurry");
     }
-    
   });
 });
 
-
-//appear on scroll
+//animate on scroll
 AOS.init({
-  once: true
+  once: true,
 });
-
-
 
 //particle js
 window.onload = () => {
   Particles.init({
-  
-  selector: '.particle',
+    selector: ".particle",
 
-  color: ["#accbee","#feada6", "#bdc2e8", "#fdcbf1", "#fccb90", "#d57eeb", "#f093fb", "#4facfe", "#43e97b", "#6f86d6"],
+    color: [
+      "#accbee",
+      "#feada6",
+      "#bdc2e8",
+      "#fdcbf1",
+      "#fccb90",
+      "#d57eeb",
+      "#f093fb",
+      "#4facfe",
+      "#43e97b",
+      "#6f86d6",
+    ],
 
-  speed: .3,
+    speed: 0.3,
 
-  sizeVariatiuons: 10,
+    sizeVariatiuons: 10,
 
-  connectParticles: true,
+    connectParticles: true,
 
-  minDistance: 50,
+    minDistance: 50,
 
-  maxParticles: 200,
+    maxParticles: 200,
 
-  responsive: [{
-
-    breakpoint: 760,
-    options : {
-      maxParticles: 100
-    }
-  }
-  ]
-  
-
-    });
-  };
-
+    responsive: [
+      {
+        breakpoint: 760,
+        options: {
+          maxParticles: 100,
+        },
+      },
+    ],
+  });
+};
 
 // /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 // let prevScrollpos = window.pageYOffset;
@@ -95,5 +94,5 @@ window.onload = () => {
 //     }
 //     prevScrollpos = currentScrollPos;
 //   }
-  
+
 // }
